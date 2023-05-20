5786ecdca4942f7957add857b4aa216a9fc7a8e0
Amrit Anand Singh <amritanandsingh@Amrits-MacBook-Air.local>
2023-05-20 12:11:38 +0530

    Working fine

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
-const sassMiddleware =require('node-sass-middleware');
-
-
+//const sassMiddleware =require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
+/*
 app.use(sassMiddleware({
     src: './assets/scss',
     dest: './assets/css',
@@ -19,6 +20,7 @@ app.use(sassMiddleware({
     outputStyle: 'extended',
     prefix: '/css'
 }));
+*/
 app.use(express.urlencoded());
 
 app.use(cookieParser());
@@ -61,9 +63,9 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.use(passport.setAuthenticatedUser);
-
+app.use(flash());  
+app.use(customMware.setFlash);
 // use express router
 app.use('/', require('./routes'));
 
