4c4a1f230419db31d6b6f99c27d4b417fbc8614e
Amrit Anand Singh <amritanandsingh@Amrits-MacBook-Air.local>
2023-05-31 16:02:35 +0530

    chating is working

--- index.js
+++ index.js
@@ -17,10 +17,18 @@ const MongoStore = require('connect-mongo')(session);
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 
+// setup chat server to be used with socket io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5001);
+console.log("chat server is lisening on port 5000");
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
     extended: false
 }))
+
+
 /*
 app.use(sassMiddleware({
     src: './assets/scss',
